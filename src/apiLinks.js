510a5fe8cff651ddd65a1fa9aaeee233477b8302
nimaester <nimaester@gmail.com>
2021-03-31 18:19:05 -0700

    got the api key and url

--- src/apiLinks.js
+++ src/apiLinks.js
@@ -1,3 +1,5 @@
 let imageURL = "https://image.tmdb.org/t/p/w1280";
 let searchURL = `https://api.themoviedb.org/3/search/company?api_key=${process.env.REACT_APP_TMDB_API}&query=`;
 let popularURL = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_API}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
+let newMoviesURL = `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_API}&language=en-US&page=1`;
+let nowPlayingURL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_TMDB_API}&language=en-US&page=1`;
