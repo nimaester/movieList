48597b588e1d26a2c981c1c14dc06ef59a47f9ee
nimaester <nimaester@gmail.com>
2021-04-04 23:17:44 -0700

    added logo

--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,13 @@
 import React from "react";
+import logo from "./logo.png";
 
 const Nav = () => {
   return (
     <nav>
       <ul className='nav'>
-        <li>LOGO</li>
+        <li>
+          <img src={logo} alt='logo' />
+        </li>
         <form>
           <input placeholder='Type a Movie title' />
           <button>Search</button>
