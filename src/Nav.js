0b84db8858467b66367005ff3ee8324ccd1a9480
nimaester <nimaester@gmail.com>
2021-04-13 23:46:03 -0700

    removed background color

--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,11 @@
 import React from "react";
-import logo from "./logo.png";
+// import logo from "./logo.png";
 
 const Nav = () => {
   return (
     <nav>
       <ul className='nav'>
-        <li>
-          <img src={logo} alt='logo' />
-        </li>
+        <li></li>
         <form>
           <input placeholder='Type a Movie title' />
           <button>Search</button>
